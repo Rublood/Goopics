057c4e063d2b6821e478bea0661b45d0be79bf85
remi <ciesielski.remi@gmail.com>
2018-02-20 18:22:23 +0100

    changement id xml

--- app/src/main/java/net/goopics/goopics/Menu.java
+++ app/src/main/java/net/goopics/goopics/Menu.java
@@ -1,13 +1,28 @@
 package net.goopics.goopics;
 
+import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class Menu extends AppCompatActivity {
-
-    @Override
+public class Menu extends ListActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
+
+        String[] values = new String[] { "Device",
+                "Géo localisation", "Accéléromètre",
+                "Navigateur internet", "Dialogues", "Album photos",
+                "Connexion réseau", "Gestion des fichiers",
+                "Carnet de contacts" };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.rowlayout, values);
+        setListAdapter(adapter);
+    }
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        Toast.makeText(this, "Position : " + position, Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/net/goopics/goopics/Upload.java
+++ app/src/main/java/net/goopics/goopics/Upload.java
@@ -2,12 +2,17 @@ package net.goopics.goopics;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Upload extends AppCompatActivity {
+    private Button menu = null;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload);
+        menu = (Button) findViewById(R.id.);
     }
 }
